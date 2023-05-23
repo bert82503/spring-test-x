55f3a1a950e48405f7bfea5b20c419eee8a9ca89
bert82503 <bert825_work@163.com>
2023-05-24 01:13:31 +0800

    integrate testng-mockito-assertj in UserServiceTestngTest

--- src/test/java/com/test/service/UserServiceTestngTest.java
+++ src/test/java/com/test/service/UserServiceTestngTest.java
@@ -1,33 +1,40 @@
 package com.test.service;
 
-import com.test.AbstractTestNGUnitTests;
 import com.test.tutorial.bean.User;
 import com.test.tutorial.dal.UserDao;
 import com.test.tutorial.service.UserService;
 import com.test.tutorial.service.impl.UserServiceImpl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.quality.Strictness;
+import org.mockito.testng.MockitoSettings;
+import org.mockito.testng.MockitoTestNGListener;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit test of {@link UserService}.
  *
- * @author Bert Lee
+ * @author Edward Lee
  * @version 2014-7-25
+ * @version 2023-5-24
  */
-public class UserServiceUnitTest extends AbstractTestNGUnitTests {
+@SuppressWarnings("unused")
+@Listeners(MockitoTestNGListener.class)
+@MockitoSettings(strictness = Strictness.WARN)
+public class UserServiceTestngTest {
 
     // tested service
     @InjectMocks
     private final UserService userService = new UserServiceImpl();
+    // 注意：服务实现类声明！(不建议这样使用！)
 //    @InjectMocks
-//    private UserServiceImpl userService; // 注意：服务实现类声明！(不建议这样使用！)
+//    private UserServiceImpl userService;
 
     // mocked service (被依赖的服务)
     // 9. Shorthand for mocks creation - @Mock annotation
@@ -47,17 +54,14 @@ public class UserServiceUnitTest extends AbstractTestNGUnitTests {
 
 
     /**
-     * <ul>
-     *   <li>
-     *       Mockito:
-     *       <a href="https://static.javadoc.io/org.mockito/mockito-core/2.8.9/org/mockito/Mockito.html#2">
-     *           2. How about some stubbing?</a>
-     *   </li>
-     *   <li>
-     *       <a href="http://joel-costigliola.github.io/assertj/">
-     *           AssertJ - Fluent assertions for java</a>
-     *   </li>
-     * </ul>
+     * <pre>
+     * Mockito:
+     * <a href="http://site.mockito.org/mockito/docs/current/org/mockito/Mockito.html#2">
+     *     2. How about some stubbing?</a>
+     *
+     * <a href="http://joel-costigliola.github.io/assertj/">
+     *     AssertJ - Fluent assertions for java</a>
+     * </pre>
      */
     @Test(dataProvider = "getUserNameTestData")
     public void getUserName(long userId, User user, String expected) {
@@ -82,7 +86,7 @@ public class UserServiceUnitTest extends AbstractTestNGUnitTests {
                 {24L, null, ""},
                 {3L, new User(3L, ""), ""},
                 {10L, new User(10L, "Edward Lee"), "Edward Lee"},
-                {23L, new User(23L, "行乐@!~#$%^&"), "行乐@!~#$%^&"},
+                {23L, new User(23L, "广益@!~#$%^&"), "广益@!~#$%^&"},
         };
     }
 
@@ -102,13 +106,15 @@ public class UserServiceUnitTest extends AbstractTestNGUnitTests {
     private static Object[][] updateUserNameTestData() {
         return new Object[][]{
                 // param invalid
-                {-1L, "Edward Lee", 0, false}, // userId
-                {0L, "行乐@!~", 1, false},
-                {123L, null, 1, false}, // userName
+                // userId
+                {-1L, "Edward Lee", 0, false},
+                {0L, "广益@!~", 1, false},
+                // userName
+                {123L, null, 1, false},
                 {456L, "", 0, false},
                 //
                 {23L, "Edward Lee", 0, false},
-                {24L, "行乐@!~", 1, true},
+                {24L, "广益@!~", 1, true},
         };
     }
 }
